abb0a5f60c70cc66e7561524e36bf49f32b003f5
ShilpeshThakur <thakur.shilpesh007@gmail.com>
2025-07-28 17:38:40 +0530

    palying with multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,29 +2,40 @@ const express = require("express")
 
 const app = express();
 
-app.get("/user",(req,res)=>{
-    res.send({
-        username:"Shilpesh",
-        lastname:"Thakur"
-    })
-})
+app.use("/route", [rh1,rh2],rh3,rh4)
 
-app.post("/user",(req,res)=>{
-    console.log("User saved to database")
-    res.send("data successfully saved to database")
-})
+app.use("/user",
+    (req,res,next)=>{
+        // route handler
+        console.log("handling the route 1")
+        next()
+    },
+    (req,res,next)=>{
+         // route handler
+        console.log("handling the route 2")
+        // res.send("2nd response")
+        next()
+    },
+    (req,res,next)=>{
+         // route handler
+        console.log("handling the route 3")
+        // res.send("3rd response")
+        next()
+    },
+    (req,res,next)=>{
+         // route handler
+        console.log("handling the route 4")
+        // res.send("4th response")
+        next()
+    }
+)
 
-app.delete("/user",(req,res)=>{
-    console.log("User deleted to database")
-    res.send("data successfully deleted from database")
-})
-
-app.use("/test",(req,res)=>{
-    res.send("Namaste dashboard");
+app.use((req,res)=>{
+    res.send("response 5")
 })
 
 app.listen(7777,()=>{
-    console.log("Server is listening on port 7777")
+    console.log("Server is listening on port 7777...")
 })
 
 
